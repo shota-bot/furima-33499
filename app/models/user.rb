1789eb1ea0e1658850d7e8d8258e16d5a0085d5f
shota aoki <shotaaoki@mbp.local>
2021-01-04 19:05:01 +0900

    userモデルのバリデーションの記述を変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,39 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates :password, presence: true,
-                        format: {
-                          with: PASSWORD_REGEX, 
-                          message: "は英字と数字の両方を含めて設定してください"
-                        } 
-
-
   NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々]/.freeze
-  validates :last_name, presence: true,
-                        format: {
-                          with: NAME_REGEX,
-                          message: "は全角（漢字・ひらがな・カタカナ）で入力してください"
-                        }
-  validates :first_name, presence: true,
-                          format: {
-                           with: NAME_REGEX,
-                           message: "は全角（漢字・ひらがな・カタカナ）で入力してください"
-                          }
-
   NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/.freeze
-  validates :last_name_kana, presence: true,
-                              format: {
-                               with: NAME_KANA_REGEX,
-                               message: "は全角カタカナで入力してください"
-                              }
-  validates :first_name_kana, presence: true,
-                              format: {
-                                with: NAME_KANA_REGEX,
-                                message: "は全角カタカナで入力してください"
-                              }
 
-  validates :birthday, presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+    validates :password, format: { with: PASSWORD_REGEX, message: "は英字と数字の両方を含めて設定してください" }
+    validates :last_name, :first_name, format: { with: NAME_REGEX, message: "は全角（漢字・ひらがな・カタカナ）で入力してください" }
+    validates :last_name_kana, :first_name_kana, format: { with: NAME_KANA_REGEX, message: "は全角カタカナで入力してください" }
+  end 
+
 end
