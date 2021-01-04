c4aea65ba66f15cf4d5a9bb52dda08220d37f119
shota aoki <shotaaoki@mbp.local>
2021-01-05 08:27:54 +0900

    itemsテーブル作成

--- db/migrate/20210104102711_create_items.rb
+++ db/migrate/20210104102711_create_items.rb
@@ -2,6 +2,15 @@ class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
 
+      t.string     :name           , null: false
+      t.text       :text           , null: false
+      t.integer    :price          , null: false
+      t.references :user           , null: false, foreign_key: true
+      t.integer    :category_id    , null: false
+      t.integer    :status_id      , null: false
+      t.integer    :postage_id     , null: false
+      t.integer    :prefectures_id , null: false
+      t.integer    :day_id         , null: false
       t.timestamps
     end
   end
