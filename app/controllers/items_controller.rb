1173bad7ec393e23c9de2bf04e545ae5dd59f59b
shota aoki <shotaaoki@mbp.local>
2021-01-06 18:06:52 +0900

    削除機能にif文を追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -34,8 +34,11 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item.destroy
-    redirect_to root_path
+    if current_user.id == @item.user_id
+      @item.destroy
+    end
+      redirect_to root_path
+
   end
 
   private
