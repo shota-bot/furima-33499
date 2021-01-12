7d414d373ce22c8b0d73e037f120b8f4591de940
shota aoki <shotaaoki@mbp.local>
2021-01-12 19:00:45 +0900

    プレビュー機能の修正

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -7,16 +7,20 @@ if (document.URL.match( /new/ ) || document.URL.match( /edit/ )){
 
       const blobImage = document.createElement('img');
       blobImage.setAttribute('src', blob);
+      blobImage.setAttribute('width', 200);
+      blobImage.setAttribute('height', 200);
+
 
       imageElement.appendChild(blobImage);
       ImageList.appendChild(imageElement);
     }
 
     document.getElementById('item-image').addEventListener('change', function(e){
-      const imageContent = document.querySelector('img');
+      const imageContent = document.querySelectorAll('img')[1];
       if (imageContent) {
         imageContent.remove();
       }
+
       const file = e.target.files[0];
       const blob = window.URL.createObjectURL(file);
 
